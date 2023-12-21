accbcc724fc2a2e61f0610a67abec618c768555a
Braian <braiannickolas12@gmail.com>
2023-12-21 10:43:06 -0300

    if e Else
    Aula151

--- scripts.js
+++ scripts.js
@@ -1,4 +1,13 @@
-// Control flow
+// if...else
 
-console.log('a')
-console.log('b')
+let temperature = 40
+let highTemperature = temperature >= 37.5
+let mediumTemperature = temperature < 37.5 && temperature >= 37
+
+if (highTemperature) {
+  console.log('Febre alta')
+} else if (mediumTemperature) {
+  console.log('febre moderada')
+} else {
+  console.log('saudavel')
+}
