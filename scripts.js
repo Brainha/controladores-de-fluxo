3ac4683d5cad228c4d11e63c27eb1d0c9452f44e
Braian <braiannickolas12@gmail.com>
2024-01-02 08:54:20 -0300

    Aula 153
    Throw e try

--- scripts.js
+++ scripts.js
@@ -1,17 +1,2 @@
-// throw
-
-function sayMyName(name = '') {
-  if (name === '') {
-    throw 'Nome e obrigatorio'
-  }
-  console.log(name)
-}
-
-try {
-  sayMyName('braio')
-} catch (e) {
-  console.log(e)
-}
-
-console.log('apos a funcao de erro')
-// try...catch
+// Estrutura de repeticao
+// for
